560bc0b8c2eb2fbdf9c2a804900178361380b480
Daan <daan.van.es@gmail.com>
2016-06-07 14:56:01 +0200

    updated readme

--- NormalizationModelofAttention.py
+++ NormalizationModelofAttention.py
@@ -4,11 +4,6 @@
 Created by Daan van Es on July 06 2016
 Copyright (c) 2016 DE. All rights reserved.
 
-This is a python implementation of the Reynolds and Heeger (2009) Neuron
-Normalisation model of attention. This implementation is based upon
-the matlab code that the authors provided upon publication, which is available
-from Reynolds website at: 
-http://www.snl-r.salk.edu/~reynolds/Normalization_Model_of_Attention/
 """
 
 import numpy as np
